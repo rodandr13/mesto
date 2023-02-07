9ee10793193310c640ea075f5f610cedc3b5dde6
andrey <rodandr13@gmail.com>
2023-02-07 21:57:43 +0300

    feat: add edit profile script

--- scripts/index.js
+++ scripts/index.js
@@ -5,26 +5,24 @@ let profileJob = document.querySelector(".profile__subheader");
 let popupEditProfile = document.querySelector(".popup");
 let formElement = document.querySelector(".popup__form");
 let popupEditProfileCloseBtn = document.querySelector(".popup__close-button");
-let nameInput = document.querySelector();
-let jobInput = 0;
+let nameInput = document.querySelector(".form__input_type_name");
+let jobInput = document.querySelector(".form__input_type_job");
 
 function popupEditProfileOpen() {
   popupEditProfile.classList.add("popup_opened");
-
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function popupEditProfileClose() {
   popupEditProfile.classList.remove("popup_opened");
 }
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupEditProfileClose();
 }
 
 editProfileBtn.addEventListener('click', popupEditProfileOpen);
