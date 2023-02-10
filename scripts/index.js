85b3a55c55dccb5301189f5e3887137827d65b12
andrey <rodandr13@gmail.com>
2023-02-10 17:41:41 +0300

    fix: replace quotes

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-let editProfileBtn = document.querySelector(".profile__edit-button");
-let profileName = document.querySelector(".profile__header");
-let profileJob = document.querySelector(".profile__subheader");
+let editProfileBtn = document.querySelector('.profile__edit-button');
+let profileName = document.querySelector('.profile__header');
+let profileJob = document.querySelector('.profile__subheader');
 
-let popupEditProfile = document.querySelector(".popup");
-let formElement = document.querySelector(".popup__form");
-let popupEditProfileCloseBtn = document.querySelector(".popup__close-button");
-let nameInput = document.querySelector(".form__input_type_name");
-let jobInput = document.querySelector(".form__input_type_job");
+let popupEditProfile = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let popupEditProfileCloseBtn = document.querySelector('.popup__close-button');
+let nameInput = document.querySelector('.form__input_type_name');
+let jobInput = document.querySelector('.form__input_type_job');
 
 function popupEditProfileOpen() {
-  popupEditProfile.classList.add("popup_opened");
+  popupEditProfile.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
 function popupEditProfileClose() {
-  popupEditProfile.classList.remove("popup_opened");
+  popupEditProfile.classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
