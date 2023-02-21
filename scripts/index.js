2d54858e649af4aab5cb1312d1136eec5b140bdb
andrey <rodandr13@gmail.com>
2023-02-21 21:51:04 +0300

    feat: add remove card function

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,16 @@ let popupEditProfileCloseBtn = document.querySelector('.popup__close-button');
 let nameInput = document.querySelector('.form__input_type_name');
 let jobInput = document.querySelector('.form__input_type_job');
 
+let elements = document.querySelectorAll('.element');
+
+function removeElem(event) {
+  event.target.closest('.element').remove();
+}
+
+elements.forEach(function (elem) {
+  elem.addEventListener('click', removeElem);
+});
+
 function popupEditProfileOpen() {
   popupEditProfile.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
