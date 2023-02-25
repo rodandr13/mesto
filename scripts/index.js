a6728d67d420daeafecd2222d949c46c3067d808
andrey <rodandr13@gmail.com>
2023-02-25 21:07:47 +0300

    fix: edit close open popup

--- scripts/index.js
+++ scripts/index.js
@@ -31,24 +31,28 @@ const popupAddElem = document.querySelector('.popup_type_add-place');
 const popupAddElemFormSubmit = document.querySelector('.popup__form_type_add-place');
 const imageName = popupAddElem.querySelector('.form__input_type_image-name');
 const imageLink = popupAddElem.querySelector('.form__input_type_image-link');
-const popupAddElemCloseBtn = popupAddElem.querySelector('.popup__close-button');
 
 const popupFullImage = document.querySelector('.popup_type_image');
 const fullImage = popupFullImage.querySelector('.popup__image');
 const fullImageCaption = popupFullImage.querySelector('.popup__image-caption')
-const popupFullImageCloseBtn = popupFullImage.querySelector('.popup__close-button');
 
 const editProfileBtn = document.querySelector('.profile__edit-button');
 const popupEditProfile = document.querySelector('.popup_type_profile');
 const formProfile = popupEditProfile.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__header');
 const profileJob = document.querySelector('.profile__subheader');
-const popupEditProfileCloseBtn = document.querySelector('.popup__close-button');
 const nameInput = document.querySelector('.form__input_type_name');
 const jobInput = document.querySelector('.form__input_type_job');
 
 const elementTemplate = document.querySelector('#element').content;
 const elementsList = document.querySelector('.elements__list');
+
+const closeButtons = document.querySelectorAll('.popup__close-button');
+
+closeButtons.forEach(button => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+})
 
 function generateElements(elem) {
   const element = elementTemplate.querySelector('.element').cloneNode(true);
@@ -68,11 +72,8 @@ function openPopupImage(event) {
   fullImage.src = event.target.src;
   fullImage.alt = 'Фотография: ' + imageCaption;
   fullImageCaption.textContent = imageCaption;
-  openPopupFullImage();
-}
-
-function openPopupFullImage() {
-  popupFullImage.classList.toggle('popup_opened');
+  const popup = fullImage.closest('.popup');
+  openPopup(popup);
 }
 
 function removeElem(event) {
@@ -83,45 +84,41 @@ function likeToggle(event) {
   event.target.classList.toggle('element__button_like-active');
 }
 
-function popupAddElemToggle(event) {
-  popupAddElem.classList.toggle('popup_opened')
-}
-
-function popupEditProfileOpen() {
-  popupEditProfile.classList.add('popup_opened');
+function openEditProfile() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-}
-
-function popupEditProfileClose() {
-  popupEditProfile.classList.remove('popup_opened');
+  openPopup(popupEditProfile);
 }
 
 function handleFormSubmit(event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupEditProfileClose();
+  const popup = event.target.closest('.popup');
+  closePopup(popup);
 }
 
 function handleFormAddElementSubmit(event) {
   event.preventDefault();
   const element = {'name': imageName.value, 'link': imageLink.value};
   generateElements(element);
-  popupAddElemToggle();
+  const popup = event.target.closest('.popup');
+  closePopup(popup);
   event.target.reset();
 }
 
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
 popupAddElemFormSubmit.addEventListener('submit', handleFormAddElementSubmit);
-
-popupFullImageCloseBtn.addEventListener('click', openPopupFullImage);
-
-editProfileBtn.addEventListener('click', popupEditProfileOpen);
-popupEditProfileCloseBtn.addEventListener('click', popupEditProfileClose);
+editProfileBtn.addEventListener('click', openEditProfile);
+addElemBtn.addEventListener('click', () => openPopup(popupAddElem));
 formProfile.addEventListener('submit', handleFormSubmit);
-
-addElemBtn.addEventListener('click', popupAddElemToggle);
-popupAddElemCloseBtn.addEventListener('click', popupAddElemToggle);
 
 function initialData() {
   initialCards.forEach(function (elem) {
