767d66d2e06ceffafd03fc688189b7a9f150ff82
andrey <rodandr13@gmail.com>
2023-03-04 21:16:00 +0300

    feat: form validation

---
+++ scripts/validation.js
@@ -0,0 +1,58 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.classList.add('form__input-error_active');
+  errorElement.textContent = errorMessage;
+}
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+}
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+const hasInvalidInput = inputList => {
+  return inputList.some(inputElement => !inputElement.validity.valid);
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('form__button_inactive');
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove('form__button_inactive');
+    buttonElement.disabled = '';
+  }
+}
+
+const setEventListener = formElement => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector('.form__button');
+  inputList.forEach(inputElement => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach(formElement => {
+    formElement.addEventListener('submit', evt => {
+      evt.preventDefault();
+    })
+    setEventListener(formElement);
+  });
+}
+
+enableValidation();
